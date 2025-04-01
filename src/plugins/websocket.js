924bc45f0e6920a75354794d9447bbcc514e668e
kaunghtunthant <kaung@xanits.com>
2025-04-01 21:48:14 +0400

    broadcaster changed from pusher to reverb

--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -1,12 +1,9 @@
 import Echo from "laravel-echo";
-import Pusher from "pusher-js"; // Import Pusher
-
-window.Pusher = Pusher;
 
 window.Echo = new Echo({
-  broadcaster: "pusher",
-  key: process.env.VUE_APP_PUSHER_KEY,
-  cluster: process.env.VUE_APP_PUSHER_CLUSTER,
+  broadcaster: process.env.VUE_APP_WEBSOCKET_BROADCASTER,
+  key: process.env.VUE_APP_WEBSOCKET_KEY,
+  cluster: process.env.VUE_APP_WEBSOCKET_CLUSTER,
   wsHost: process.env.VUE_APP_WEBSOCKET_HOST, // Explicitly set wsHost
   wsPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wsPort
   wssPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wssPort
