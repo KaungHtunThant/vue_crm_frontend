e3e9e89b7906da039024565c2bb4d0889c667a0e
kaunghtunthant <kaunghtunthant@gmail.com>
2025-04-02 11:19:08 +0400

    enable wss

--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -16,16 +16,16 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
   broadcaster: process.env.VUE_APP_WEBSOCKET_BROADCASTER,
-  host: `ws://${process.env.VUE_APP_WEBSOCKET_HOST}:${process.env.VUE_APP_WEBSOCKET_PORT}`,
+  host: `wss://${process.env.VUE_APP_WEBSOCKET_HOST}:${process.env.VUE_APP_WEBSOCKET_PORT}`,
   key: process.env.VUE_APP_WEBSOCKET_KEY,
   cluster: process.env.VUE_APP_WEBSOCKET_CLUSTER,
   wsHost: process.env.VUE_APP_WEBSOCKET_HOST, // Explicitly set wsHost
   wsPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wsPort
-  // wssPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wssPort
+  wssPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wssPort
   forceTLS: process.env.VUE_APP_WEBSOCKET_TLS === "true", // Ensure this matches your environment
   disableStats: true,
   encrypted: false,
-  enabledTransports: ["ws"], // Ensure only WebSocket transports are used
+  enabledTransports: ["ws, wss"], // Ensure only WebSocket transports are used
   authEndpoint: `${process.env.VUE_APP_BACKEND_URL}/broadcasting/auth`, // Set the correct backend URL
   auth: {
     headers: {
