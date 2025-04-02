60dad9a063daf525207ed018e8171cf081275621
kaunghtunthant <kaunghtunthant@gmail.com>
2025-04-02 12:45:38 +0400

    websocket updated

--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -25,11 +25,12 @@ window.Echo = new Echo({
   forceTLS: process.env.VUE_APP_WEBSOCKET_TLS === "true", // Ensure this matches your environment
   disableStats: true,
   encrypted: false,
-  enabledTransports: ["ws, wss"], // Ensure only WebSocket transports are used
+  enabledTransports: ["ws", "wss"], // Ensure only WebSocket transports are used
   authEndpoint: `${process.env.VUE_APP_BACKEND_URL}/broadcasting/auth`, // Set the correct backend URL
   auth: {
     headers: {
       Authorization: `Bearer ${Cookies.get("authToken")}`,
+      Origin: window.location.origin, // Add the Origin header
     },
   },
 });
