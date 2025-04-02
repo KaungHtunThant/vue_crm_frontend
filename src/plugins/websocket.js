948abdb761f3a4279165e12507f6de2a650efa1c
kaunghtunthant <kaunghtunthant@gmail.com>
2025-04-02 10:59:58 +0400

    disable wss

--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -21,16 +21,15 @@ window.Echo = new Echo({
   cluster: process.env.VUE_APP_WEBSOCKET_CLUSTER,
   wsHost: process.env.VUE_APP_WEBSOCKET_HOST, // Explicitly set wsHost
   wsPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wsPort
-  wssPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wssPort
+  // wssPort: process.env.VUE_APP_WEBSOCKET_PORT, // Explicitly set wssPort
   forceTLS: process.env.VUE_APP_WEBSOCKET_TLS === "true", // Ensure this matches your environment
   disableStats: true,
   encrypted: false,
-  enabledTransports: ["ws", "wss"], // Ensure only WebSocket transports are used
+  enabledTransports: ["ws"], // Ensure only WebSocket transports are used
   authEndpoint: `${process.env.VUE_APP_BACKEND_URL}/broadcasting/auth`, // Set the correct backend URL
   auth: {
     headers: {
       Authorization: `Bearer ${Cookies.get("authToken")}`,
-      Origin: window.location.origin, // Add the Origin header
     },
   },
 });
