835412056526d2e1702a9b8842a16c435e30e9e0
Kaung Htun Thant <kaunghtunthant@gmail.com>
2025-11-12 17:31:39 +0400

    added headers to salary service api

--- src/plugins/services/salaryService.js
+++ src/plugins/services/salaryService.js
@@ -1,56 +1,74 @@
 import axios from "@/plugins/axios";
 import Cookies from "js-cookie";
 
-const token = Cookies.get("authToken");
-
-export const payingLevels = () =>
-  axios.get(`/commission-packages/`, {
+export const payingLevels = async () => {
+  const token = Cookies.get("authToken");
+  return await axios.get(`/commission-packages/`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-export const payingLevel = (id) =>
-  axios.get(`/${id}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+};
 
-export const createCommissionPackage = (data) =>
-  axios.post(`/commission-packages`, data, {
+export const payingLevel = async (id) => {
+  const token = Cookies.get("authToken");
+  return await axios.get(`/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const updateCommissionPackage = (id, data) =>
-  axios.put(`/commission-packages/${id}`, data, {
+export const createCommissionPackage = async (data) => {
+  const token = Cookies.get("authToken");
+  return await axios.post(`/commission-packages`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const deletePackage = (id) =>
-  axios.delete(`/commission-packages/${id}`, {
+export const updateCommissionPackage = async (id, data) => {
+  const token = Cookies.get("authToken");
+  return await axios.put(`/commission-packages/${id}`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const deductiontypes = () =>
-  axios.get(`/deductions/`, {
+export const deletePackage = async (id) => {
+  const token = Cookies.get("authToken");
+  return await axios.delete(`/commission-packages/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const getUserDeductions = (id) =>
-  axios.get(`/deductions/${id}`, {
+export const deductiontypes = async () => {
+  const token = Cookies.get("authToken");
+  return await axios.get(`/deductions/`, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const saveDeduction = (data) =>
-  axios.post(`/deductions/`, data, {
+export const getUserDeductions = async (id) => {
+  const token = Cookies.get("authToken");
+  return await axios.get(`/deductions/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
 
-export const deleteDeduction = (id) =>
-  axios.delete(`/deductions/${id}`, {
+export const saveDeduction = async (data) => {
+  const token = Cookies.get("authToken");
+  return await axios.post(`/deductions/`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
+};
+
+export const deleteDeduction = async (id) => {
+  const token = Cookies.get("authToken");
+  return await axios.delete(`/deductions/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
 
 export const calculatecommission = async (id) => {
-  return axios.get(`/deductions/getsalary/${id}`, {
+  const token = Cookies.get("authToken");
+  return await axios.get(`/deductions/getsalary/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
