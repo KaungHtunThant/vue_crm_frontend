e9d992c210db1387d31a4e8e1d27a572db0dfdf0
Kaung Htun Thant <kaunghtunthant@gmail.com>
2025-11-12 17:26:06 +0400

    added headers to salary service api

--- src/plugins/services/salaryService.js
+++ src/plugins/services/salaryService.js
@@ -1,16 +1,56 @@
 import axios from "@/plugins/axios";
+import Cookies from "js-cookie";
 
-export const payingLevels = () => axios.get(`/commission-packages/`);
-export const payingLevel = (id) => axios.get(`/${id}`);
+const token = Cookies.get("authToken");
+
+export const payingLevels = () =>
+  axios.get(`/commission-packages/`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+export const payingLevel = (id) =>
+  axios.get(`/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
 export const createCommissionPackage = (data) =>
-  axios.post(`/commission-packages`, data);
+  axios.post(`/commission-packages`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
 export const updateCommissionPackage = (id, data) =>
-  axios.put(`/commission-packages/${id}`, data);
-export const deletePackage = (id) => axios.delete(`/commission-packages/${id}`);
-export const deductiontypes = () => axios.get(`/deductions/`);
-export const getUserDeductions = (id) => axios.get(`/deductions/${id}`);
-export const saveDeduction = (data) => axios.post(`/deductions/`, data);
-export const deleteDeduction = (id) => axios.delete(`/deductions/${id}`);
+  axios.put(`/commission-packages/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deletePackage = (id) =>
+  axios.delete(`/commission-packages/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deductiontypes = () =>
+  axios.get(`/deductions/`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const getUserDeductions = (id) =>
+  axios.get(`/deductions/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const saveDeduction = (data) =>
+  axios.post(`/deductions/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteDeduction = (id) =>
+  axios.delete(`/deductions/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
 export const calculatecommission = async (id) => {
-  return axios.get(`/deductions/getsalary/${id}`);
+  return axios.get(`/deductions/getsalary/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 };
