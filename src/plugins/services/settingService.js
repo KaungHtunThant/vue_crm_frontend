a815c8529d06dd33b77aa155c9c20eba58d0292f
Jawad Khan <khanjawad89@gmail.com>
2025-12-25 10:27:44 +0400

    case letter change

--- src/plugins/services/settingService.js
+++ src/plugins/services/settingService.js
@@ -5,5 +5,5 @@ export const generateOTP = () => axios.get("/settings/otp/generate");
 export const getServerVersion = () => axios.get("/settings/version");
 
 export const getCustomTask = () => {
-  return axios.get("/settings/CustomTask");
+  return axios.get("/settings/customTask");
 };
